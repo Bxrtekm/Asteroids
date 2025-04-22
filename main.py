044287f5de7dedf589e723ab2323ecb669904fd6
Bxrtekm <xxwxg@proton.me>
2025-04-22 20:06:13 +0200

    Added full player movement and title, grouped updatable and drawable objects in containers

--- main.py
+++ main.py
@@ -5,17 +5,27 @@ from player import Player
 def main():
     pygame.init
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Asteroids")
     clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        updatable.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
         
         #Limiting to 60 FPS
